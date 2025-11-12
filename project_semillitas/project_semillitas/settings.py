c81b0e476cade41730112f7f4e84dfff02eb1857
marialas <mcabezassalamanca@gmail.com>
2025-11-12 16:32:57 -0500

    feat: add dockerfile to deploy

--- project_semillitas/project_semillitas/settings.py
+++ project_semillitas/project_semillitas/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
